6d196cfe8a6f1475a03822fddbcf2b64e75625f8
Andriacoder124 <andriaisfree@gmail.com>
2024-06-14 18:20:51 +0400

    Add files via upload

---
+++ aramgoniasworadgameketamaraok/script.js
@@ -0,0 +1,32 @@
+
+
+
+
+let ricxvebi = [ 10,11,12,13,14,15 ];
+
+if (ricxvebi[0] == 10) {
+    console.log(10)
+}
+else if (ricxvebi[1] == 11){
+    console.log(11)
+}
+else if (ricxvebi[2] == 12) {
+   console.log(12) 
+    
+}
+else if (ricxvebi[3] == 12){
+    console.log(13)
+}
+else if (ricxvebi[4] == 14){
+    console.log(14)
+}
+ else  (ricxvebi[5] == 15){
+    console.log(15)
+}
+
+
+
+let saxelebi = ['Guts','Zoro','Trunks','Jager','Friza']
+
+saxelebi.shift()
+saxelebi.push('Guts')
